0f3d562b6fefe6bebc297ee7a4655b5d2211298f
Djeeberjr <djeeberjr@gmail.com>
2025-09-15 15:43:22 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite"
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite"
+import handlebars from "vite-plugin-handlebars";
+import ctx from "./context.json"
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    handlebars({
+      context: ctx,
+      partialDirectory: resolve(__dirname, "partials"),
+    })
+  ],
+})
